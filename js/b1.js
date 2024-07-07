0a94cc39b1c33c98b6ed4719dafcd668e3c34b51
AnhHT3010 <trunganhhoang2001@gmail.com>
2024-07-07 21:02:44 +0700

    js

---
+++ js/b1.js
@@ -0,0 +1,23 @@
+// Dữ liệu: số, chữ
+const pi = 3.1493567
+// tên chuẩn của 1 biến: thì sẽ không có dấu tiếng việt, không có kí tự đặc biệt, không có dấu cách, không bắt đầu bằng số 
+pi = 123
+// Cú pháp khai báo biến
+// let <tên biến> = <giá trị>
+let x = 5
+
+// Dữ liệu: số, chữ
+// tên chuẩn của 1 biến: thì sẽ không có dấu tiếng việt, không có kí tự đặc biệt, không có dấu cách, không bắt đầu bằng số 
+// Cú pháp khai báo biến
+// let <tên biến> = <giá trị>
+
+let name = "Hoang_Anh"
+name = "Hoang Anh da ngam, cam on vi da den"
+// output
+console.log(name)
+
+// 2 kiểu dữ liệu: số, chữ bắt buộc phải nằm trong dấu nháy kép
+// tạo 2 biến lưu trữ tuổi, địa chỉ của bạn.
+
+let tuổi = "16"
+let 
